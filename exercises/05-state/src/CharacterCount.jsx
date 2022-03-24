047e9c018705bc43cbc389bc1636e65b2de585f9
Caleb Ruhm <ruhmc0619@gmail.com>
2022-03-23 20:00:22 -0400

    Homework 3.23

--- exercises/05-state/src/CharacterCount.jsx
+++ exercises/05-state/src/CharacterCount.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useState } from 'react'
 
 export default function CharacterCount( ) {
+  const [counter, setCounter] = useState('');
   return (
-    <div>
-
+    <div className='box'>
+      <textarea onChange={e => setCounter(e.target.value.length)}></textarea><br></br>
+      Character Count: {counter}
     </div>
   )
 }
