3fd3a413746bf25266c1f692e7e6e81aaec34a58
Caleb Ruhm <ruhmc0619@gmail.com>
2022-03-28 21:08:28 -0400

    idk

--- exercises/07-crud/src/App.jsx
+++ exercises/07-crud/src/App.jsx
@@ -2,18 +2,38 @@ import { useState } from "react";
 import "./App.css";
 
 const GroceryList = () => {
+
+  const handleSubmit = event => {
+    event.preventDefault();
+  }
+
+  const addItem = () => {
+    setActionItems([...actionItems])
+  }
+
+  const [item, setItem] = useState('');
+  const [price, setPrice] = useState('');
+
+  const [actionItems, setActionItems] = useState([
+
+  ])
+
   return (
     <div className="container">
       <div className="card card-body bg-light mb-2">
-        <form method="POST" className="row g-3">
+        <form onSubmit={handleSubmit} method="POST" className="row g-3">
+
           <div className="col">
             <input
               className="form-control"
               type="text"
               placeholder="Name of grocery item..."
               aria-label="Name of grocery item..."
+              value={item}
+              onChange = {e => setItem(e.target.value)}
             />
           </div>
+
           <div className="col">
             <input
               className="form-control"
@@ -22,15 +42,20 @@ const GroceryList = () => {
               step=".01"
               placeholder="Cost of grocery item..."
               aria-label="Cost of grocery item..."
+              value={price}
+              onChange = {e => setPrice(e.target.value)}
             />
           </div>
+          
           <div className="col-md-auto">
-            <button type="submit" className="btn btn-success">
+            <button onSubmit={addItem}  type="submit" className="btn btn-success">
               Add
             </button>
           </div>
+          
         </form>
       </div>
+
       <div className="card card-body border-white">
         <h1 className="h4">Grocery List</h1>
         <table className="table table-sm">
@@ -42,6 +67,20 @@ const GroceryList = () => {
             </tr>
           </thead>
           <tbody>
+            {actionItems.map((item, index) => {
+              return (
+              <tr>
+                <td>{item}</td>
+                <td>{price}</td>
+                <td>
+                  <button>
+                    &times;
+                  </button>
+                </td>
+              </tr>
+              )
+            })}
+            
             {/**
              * Complete me. (You can use something else instead of a table if you like)
              * @example
@@ -55,16 +94,20 @@ const GroceryList = () => {
              *   </td>
              * </tr>
              */}
+
           </tbody>
         </table>
+
         <p className="lead">
           <strong>Total Cost: {/* Complete me */}</strong>
         </p>
+
         <div className="d-flex justify-content-end">
           <button type="button" className="btn btn-outline-success">
             Clear
           </button>
         </div>
+
       </div>
     </div>
   );
