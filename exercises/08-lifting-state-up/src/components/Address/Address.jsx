d9225041903af8ea8c53ea1245f78b615d16c404
Caleb Ruhm <ruhmc0619@gmail.com>
2022-03-30 20:18:23 -0400

    3.30

--- exercises/08-lifting-state-up/src/components/Address/Address.jsx
+++ exercises/08-lifting-state-up/src/components/Address/Address.jsx
@@ -4,11 +4,17 @@ import AddressForm from "../AddressForm/AddressForm";
 import AddressResults from "../AddressResults/AddressResults";
 
 function App() {
-  
+
   const [displayResults, setDisplayResults] = useState(false);
   const [values,setValues] = useState({})
 
-  return <>{displayResults ? <AddressResults values={values} /> : <AddressForm setDisplayResults={setDisplayResults} values={values} setValues={setValues} />}</>;
+  return <>
+  { 
+    displayResults 
+    ? <AddressResults values={values} /> 
+    : <AddressForm setDisplayResults={setDisplayResults} values={values} setValues={setValues} />
+  } 
+  </>;
 }
 
 export default App;
