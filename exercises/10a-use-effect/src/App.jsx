bdc469e0b5852b405326c0c211ca324474b19256
Caleb Ruhm <ruhmc0619@gmail.com>
2022-04-06 20:21:32 -0400

    #10

--- exercises/10a-use-effect/src/App.jsx
+++ exercises/10a-use-effect/src/App.jsx
@@ -1,28 +1,46 @@
 // Import useEffect here
 import "./App.css";
 import ronSwansonImage from "./assests/ronswanson.png";
+import { useEffect, useState } from "react";
+import axios from 'axios'
 // import Axios (or use Fetch)
 
 function App() {
-  /**
-   * Set up your state
-   */
 
-  /**
-   * Make an AJAX call with the useEffect hook
-   */
+  const [quote, setQuote] = useState('');
+  const [error, setError] = useState('')
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+
+    window.setTimeout(() => {
+      axios('http://ron-swanson-quotes.herokuapp.com/v2/quotess')
+        .then(response => {
+          setQuote(response.data);
+        })
+        .catch(() => {
+          setError(true);
+          setQuote('')
+        })
+        .then(() => {
+        })
+        setLoading(false)
+    }, 4000)
+  }, [])
 
   return (
     <body class="bg-warning text-center">
       <img src={ronSwansonImage} alt="" width="450" height="423" class="mt-4" />
       <div class="container">
-        {/* Display a loading message */}
-        {/* Display an error message if the API fails */}
+        {loading && 
+        <h2>Loading...</h2>}
+        {error && 
+        <h2>Uh oh. Looks like you've been Swansoned!</h2>}
         <blockquote
           id="quote"
           class="blockquote bg-dark text-white border-0 mb-4"
         >
-          {/* Complete me */}
+          {quote}
         </blockquote>
       </div>
     </body>
