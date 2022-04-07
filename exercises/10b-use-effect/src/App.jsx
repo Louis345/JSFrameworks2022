cc76ba3376d753d692e442a7a5c3e44bd39967a6
Caleb Ruhm <ruhmc0619@gmail.com>
2022-04-06 20:55:31 -0400

    homework 4.6

--- exercises/10b-use-effect/src/App.jsx
+++ exercises/10b-use-effect/src/App.jsx
@@ -1,33 +1,24 @@
-// Import useEffect here
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
-// import Axios (or use Fetch)
+import axios from 'axios'
 
 function App() {
-  /**
-   * dogImages
-   * @type {Array} an array of image URLs
-   * @example
-   * [
-   *     "https://images.dog.ceo/breeds/terrier-scottish/n02097298_7694.jpg",
-   *     "https://images.dog.ceo/breeds/lhasa/n02098413_1137.jpg"
-   * ]
-   */
+  
+  const [number, setNumber] = useState(1)
   const [dogImages, setDogImages] = useState([]);
 
-  /**
-   * You may need to set something else in state
-   */
-
-  /**
-   * Make an AJAX call with the useEffect hook
-   */
+  useEffect(() => {
+    axios(`https://dog.ceo/api/breeds/image/random/${number}`)
+    .then(response => {
+      setDogImages(response.data.message)
+    })
+  }, [number])
 
   return (
     <div className="App">
       <h1>Dogs</h1>
       {/* Make me a controlled input */}
-      <select>
+      <select onChange={(e) => setNumber(e.target.value)}>
         <option value="1">1</option>
         <option value="2">2</option>
         <option value="3">3</option>
@@ -41,7 +32,7 @@ function App() {
       </select>
       <div className="container">
         {dogImages.map((dogImage, idx) => {
-          return <img key={`dog-${idx}`} height="200" src={dogImage} />;
+          return <img key={`dog-${idx}`} height="200" src={dogImage} alt=''/>;
         })}
       </div>
     </div>
