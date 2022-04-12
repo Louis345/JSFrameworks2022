19d83a3ac69e2e4a2872373b3d9490c180747548
Caleb Ruhm <ruhmc0619@gmail.com>
2022-04-11 20:51:26 -0400

    homework 4.11

--- exercises/10c-use-effect/src/App.jsx
+++ exercises/10c-use-effect/src/App.jsx
@@ -1,32 +1,45 @@
-// import useEffect
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
-// import Axios (or use Fetch)
+import axios from "axios"
 
 function App() {
-  /**
-   * Set up state and make AJAX requests here
-   */
 
+  const [info, setInfo] = useState([])
+  const [character, setCharacter] = useState('')
+
+  useEffect(() => {
+    axios(`https://rickandmortyapi.com/api/character`)
+    .then(response => {
+      setInfo(response.data.results)
+      console.log(info)
+    })
+  },[])
+
+  const handleEvent = (e) => {
+    axios(`https://rickandmortyapi.com/api/character/${e.target.value}`)
+    .then(response => {
+      setCharacter(response.data)
+    })
+  }
+  
   return (
     <div className="container">
       <div className="row text-center" id="body">
-        <h1 id="title-head">{/* Plugin character name here */}</h1>
+        <h1 id="title-head">{character.name}</h1>
         <div id="main-img">
           <a href="http://rickandmorty.wikia.com/wiki/Rick_Sanchez">
             {/* Add an alt and src to this image */}
-            <img height="250" />
+            <img height="250" alt={character.name} src={character.image}/>
           </a>
           <div className="linkfooter">
             <p>Select your favorite character</p>
-            {/* Handle event here */}
-            <select id="dropdown" type="text">
-              <option></option>
-              {/**
-               * Loop through all characters. The value should be the character id.
-               * @example in HTML
-               * <option value="2" key="character-1">Morty Smith</option>
-               */}
+            <select id="dropdown" type="text" onChange={handleEvent}>
+              {info.map((character, index) => {
+                return (
+                  <option value={character.id} key={index}>
+                  {character.name}</option>
+                )
+              })}
             </select>
           </div>
         </div>
