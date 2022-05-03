8771fed2ee5abba58e70ef200a880bf1c12658e3
matinaspatsos <matinaspatsos@gmail.com>
2022-05-02 20:52:40 -0400

    Rename to Zustand

--- exercises/17-zustand/src/Layouts/Header/Header.jsx
+++ exercises/17-zustand/src/Layouts/Header/Header.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import Tabs from "../../components/Tabs/Tabs";
 import Search from "../../components/Search/Search";
 import styles from "./Header.module.css";
-import { MdOutlineSubject } from "react-icons/md";
-import { AiFillAppstore } from "react-icons/ai";
 
 const Header = ({ onSearch, onTab }) => {
   return (
@@ -12,8 +10,6 @@ const Header = ({ onSearch, onTab }) => {
         <Tabs onTab={onTab} />
         <div className={styles.bottomContainer}>
           <Search onSearch={onSearch} />
-          <MdOutlineSubject className={styles.icon} size={30} />
-          <AiFillAppstore className={styles.icon} size={30} />
         </div>
       </div>
     </div>
